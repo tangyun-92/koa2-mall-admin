e69f95e71c0ac25a246021321779700da46af8ff
唐云 <tangyun1992@qq.com>
2021-07-21 17:26:26 +0800

    '将mysql的配置提取到config中'

--- app/config/db.js
+++ app/config/db.js
@@ -1,17 +1,13 @@
-const env = process.env.NODE_ENV
-
 let MYSQL_CONF
 
-if (env === 'dev') {
-  MYSQL_CONF = {
-    host: '172.16.11.25',
-    user: 'root',
-    password: '123456',
-    port: '3306',
-    database: 'ball'
-  }
+MYSQL_CONF = {
+  host: '172.16.11.25',
+  user: 'root',
+  password: '123456',
+  port: '3306',
+  database: 'ball',
 }
 
 module.exports = {
-  MYSQL_CONF
+  MYSQL_CONF,
 }
--- app/models/db.js
+++ app/models/db.js
@@ -1,14 +1,20 @@
 const Sequelize = require('sequelize')
+const { MYSQL_CONF } = require('../config/db')
 
-const sequelize = new Sequelize('ball', 'root', '123456', {
-  host: '172.16.11.25',
-  dialect: 'mysql',
-  // 格式化时间
-  dialectOptions: {
-    dateStrings: true,
-    typeCast: true
-  },
-  timezone: '+08:00', //改为标准时区
-})
+const sequelize = new Sequelize(
+  MYSQL_CONF.database,
+  MYSQL_CONF.user,
+  MYSQL_CONF.password,
+  {
+    host: MYSQL_CONF.host,
+    dialect: 'mysql',
+    // 格式化时间
+    dialectOptions: {
+      dateStrings: true,
+      typeCast: true,
+    },
+    timezone: '+08:00', //改为标准时区
+  }
+)
 
 module.exports = sequelize
