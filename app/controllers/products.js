5ee7bfecc795c18cf014498b3988d636f81c5eab
唐云 <tangyun1992@qq.com>
2021-08-02 16:45:35 +0800

    完成产品管理列表、增、删、改接口

---
+++ app/controllers/products.js
@@ -0,0 +1,123 @@
+/*
+ * @Author: 唐云
+ * @Date: 2021-07-25 21:48:32
+ * @Last Modified by: 唐云
+ * @Last Modified time: 2021-08-02 16:36:39
+ * 产品
+ */
+const Product = require('../models/products')
+const sequelize = require('sequelize')
+const { Op } = require('sequelize')
+const { returnCtxBody } = require('../utils/index')
+const Brand = require('../models/brands')
+const Category = require('../models/categorys')
+const SpecGroup = require('../models/spec-group')
+
+class ProductCtl {
+  // 获取产品列表
+  async find(ctx) {
+    let {
+      page = 1,
+      pageSize = 5,
+      title = '',
+      saleable = '',
+      valid = '',
+    } = ctx.request.body
+    page = Math.max(page, 1)
+    pageSize = Math.max(pageSize, 1)
+    const { count, rows } = await Product.findAndCountAll({
+      offset: (page - 1) * pageSize,
+      limit: pageSize,
+      order: ['id'],
+      where: {
+        title: {
+          [Op.like]: `${title}%`,
+        },
+        saleable: {
+          [Op.like]: `${saleable}%`,
+        },
+        valid: {
+          [Op.like]: `${valid}%`,
+        },
+      },
+      attributes: {
+        include: [
+          [sequelize.col('b.name'), 'brand_name'],
+          [sequelize.col('c.name'), 'category_name'],
+          [sequelize.col('s.name'), 'spec_group_name'],
+        ]
+      },
+      include: [
+        {
+          model: Brand,
+          as: 'b',
+          attributes: []
+        },
+        {
+          model: Category,
+          as: 'c',
+          attributes: []
+        },
+        {
+          model: SpecGroup,
+          as: 's',
+          attributes: []
+        }
+      ]
+    })
+    ctx.body = returnCtxBody({
+      data: {
+        records: rows,
+        page,
+        pageSize,
+        total: count,
+      },
+    })
+  }
+
+  // 创建/更新产品
+  async update(ctx) {
+    const { id } = ctx.request.body
+    if (id) {
+      ctx.verifyParams({
+        title: { type: 'string', require: false },
+        category_id: { type: 'number', require: false },
+        brand_id: { type: 'number', require: false },
+        spg_id: { type: 'number', require: false },
+        saleable: { type: 'number', require: false },
+        valid: { type: 'number', require: false },
+      })
+      const repeatedId = await Product.findByPk(id)
+      if (!repeatedId) {
+        ctx.throw(200, '产品不存在')
+      }
+      await Product.update(ctx.request.body, { where: { id } })
+    } else {
+      ctx.verifyParams({
+        title: { type: 'string', require: true },
+        category_id: { type: 'number', require: true },
+        brand_id: { type: 'number', require: false },
+        spg_id: { type: 'number', require: true },
+        saleable: { type: 'number', require: true },
+        valid: { type: 'number', require: true },
+      })
+      await Product.create(ctx.request.body)
+    }
+    ctx.body = returnCtxBody({})
+  }
+
+  // 删除产品
+  async delete(ctx) {
+    const { id } = ctx.request.body
+    await Product.destroy({
+      where: {
+        id: {
+          [Op.or]: id,
+        },
+      },
+    })
+    ctx.body = returnCtxBody({})
+  }
+}
+
+module.exports = new ProductCtl()
---
+++ app/models/products.js
@@ -0,0 +1,73 @@
+/*
+ * @Author: 唐云 
+ * @Date: 2021-07-25 21:49:05 
+ * @Last Modified by: 唐云
+ * @Last Modified time: 2021-08-02 16:37:39
+ * 产品模型
+ */
+const { DataTypes } = require('sequelize')
+const sequelize = require('./db')
+const Brand = require('../models/brands')
+const Category = require('../models/categorys')
+const SpecGroup = require('../models/spec-group')
+
+const Product = sequelize.define(
+  'Products',
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    sub_title: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    category_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+    brand_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: true,
+    },
+    spg_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+    saleable: {
+      type: DataTypes.TINYINT.UNSIGNED,
+      allowNull: false
+    },
+    valid: {
+      type: DataTypes.TINYINT.UNSIGNED,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: 't_spu',
+  }
+)
+
+Product.belongsTo(Brand, { foreignKey: 'brand_id', targetKey: 'id', as: 'b'})
+Product.belongsTo(Category, { foreignKey: 'category_id', targetKey: 'id', as: 'c'})
+Product.belongsTo(SpecGroup, {
+  foreignKey: 'spg_id',
+  targetKey: 'spg_id',
+  as: 's',
+})
+
+module.exports = Product
